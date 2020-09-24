e51b1882dff793c47be7afc94d9fe04467a568ed
flashing99 <enafor99@gmail.com>
2020-09-24 19:09:12 +0200

    init project

---
+++ lib/pages/start_page.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:simple_animations/simple_animations.dart';
+
+class StartPage extends StatelessWidget {
+  const StartPage({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(),
+    );
+  }
+}
