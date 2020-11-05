ebcebbcd3aa83ad6da9dc6fe3dce9c8616e8cd83
flashing99 <enafor99@gmail.com>
2020-11-05 18:33:29 +0100

    update URL images

--- lib/utils/constantes.dart
+++ lib/utils/constantes.dart
@@ -2,11 +2,6 @@ const currency = ' DA';
 
 const ROOTURL = 'http://danone.cooffa.shop';
 const BASEURL = ROOTURL + '/api/v1/clients';
-const STORAGE = 'data_app/storage/app/public/products/images/';
 
-//http://danone.cooffa.shop/data_app/storage/app/public/products/images/1.png
-
-const IMAGEURL = ROOTURL + '/' + STORAGE;
-
-//const baseUrl = 'http://192.168.1.2/laravel/aydak/public/api/v1/users/';
-//const baseUrl = 'http://aydak.dev.com/api/v1/users/';
+//http://danone.cooffa.shop/api/v1/clients/articles/images/
+const IMAGEURL = ROOTURL + ROOTURL + '/articles/images/';
