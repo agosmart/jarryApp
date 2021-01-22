5853945d46b12bf1e22947fcd4889cb33436b4c0
flashing99 <flashing99@bitbucket.org>
2021-01-22 23:50:31 +0100

    add coof adress

--- lib/utils/constantes.dart
+++ lib/utils/constantes.dart
@@ -1,10 +1,10 @@
 const currency = ' DA';
 
+// const ROOTURL = "http://danone.cooffa.shop/";
 const ROOTURL = 'https://jari.danonepromo.dz';
 const BASEURL = ROOTURL + '/api/v1/clients';
 
 //http://danone.cooffa.shop/api/v1/clients/articles/images/
-
 
 // https://jari.danonepromo.dz/api/v1/clients/familles/images/1.png
 const IMAGEICONURL = BASEURL + '/familles/images/';
